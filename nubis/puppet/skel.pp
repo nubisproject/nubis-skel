65402e9c114d8dbbca9dc04412d39674578b8a8e
Jason Crowe (Mozilla) <jcrowe@mozilla.com>
2015-07-10 15:34:21 -0700

    Add example of puppet file copy/provisioner

--- nubis/puppet/skel.pp
+++ nubis/puppet/skel.pp
@@ -1,7 +1,14 @@
-# This application specific puppet file is where we install miscelanous packages etc...
+# This application specific puppet file is where we do work that is not
+#+ already included in other puppet modules.
+#
+# There are a number of examples in here for installing packages as well
+#+ as working with files and templates.
+#
+# Feel free to remove any of these that you do not need for your project.
+#
 
-exec { "apt-get update":
-    command => "/usr/bin/apt-get update",
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'makepasswd':
@@ -12,4 +19,16 @@ package { 'makepasswd':
 package { 'git':
   ensure => present,
   require  => Exec['apt-get update'],
+}
+
+file { '/etc/update-motd.d/55-nubis-welcome':
+  source => 'puppet:///nubis/files/nubis-welcome',
+  owner => 'root',
+  group => 'root',
+  mode  => '0755',
+}
+
+exec { 'motd update':
+  command => '/bin/run-parts /etc/update-motd.d/ > /var/run/motd.dynamic',
+  require  => File['/etc/update-motd.d/55-nubis-welcome'],
 }
