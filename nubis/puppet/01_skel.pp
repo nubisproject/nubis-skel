975ea7b3bb56780746b751bb66c480385f56ae8f
Jason Crowe (Mozilla) <jcrowe@mozilla.com>
2015-10-29 13:47:23 -0700

    Make amazon-linux builds work

--- nubis/puppet/01_skel.pp
+++ nubis/puppet/01_skel.pp
@@ -7,18 +7,21 @@
 # Feel free to remove any of these that you do not need for your project.
 #
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
+exec { 'package_manager_update':
+  command => $package_manager_update_command,
+  returns => [ '0', '100' ],
 }
 
-package { 'makepasswd':
-  ensure => '1.10-9',
-  require  => Exec['apt-get update'],
+package { 'makepasswd_package':
+  ensure => $makepasswd_package_version,
+  name   => $makepasswd_package_name,
+  require  => Exec['package_manager_update'],
 }
 
-package { 'git':
-  ensure => present,
-  require  => Exec['apt-get update'],
+package { 'git_package':
+  ensure => $git_package_version,
+  name   => $git_package_name,
+  require  => Exec['package_manager_update'],
 }
 
 file { '/etc/update-motd.d/55-nubis-welcome':
@@ -28,7 +31,7 @@ file { '/etc/update-motd.d/55-nubis-welcome':
   mode  => '0755',
 }
 
-exec { 'motd update':
-  command => '/bin/run-parts /etc/update-motd.d/ > /var/run/motd.dynamic',
+exec { 'motd_update':
+  command => $motd_update_command,
   require  => File['/etc/update-motd.d/55-nubis-welcome'],
 }
--- nubis/puppet/init.pp
+++
@@ -1,7 +0,0 @@
-# Main entry for puppet
-#
-
-# Here we are simply importing a custom application puppet file.
-# Typically you will include things like Apache and other puppet modules
-# which are included in the base image.
-import 'skel.pp'
