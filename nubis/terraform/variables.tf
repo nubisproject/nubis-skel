8f9c01f6a81772e8c62ec3f32ef435ace54fb8f3
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-12 14:35:24 -0400

    Add Terraform deployment code, using nubis-terraform modules

---
+++ nubis/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "account" {}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "environment" {
+  default = "stage"
+}
+
+variable "service_name" {
+  default = "skel"
+}
+
+variable "ami" {}
