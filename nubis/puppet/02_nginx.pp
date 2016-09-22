7f78c2ecd09658dabe2a0c1f4f153b6aec30cb27
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-22 15:00:43 -0400

    Add project_name (i.e. service_name) as a service tag

--- nubis/puppet/02_nginx.pp
+++ nubis/puppet/02_nginx.pp
@@ -3,7 +3,7 @@ $port = 80
 # Install our service discovery handy helper
 include nubis_discovery
 nubis::discovery::service { "$project_name":
-  tags => [ 'nginx' ],
+  tags => [ 'nginx', '%%NUBIS_PROJECT%%' ],
   port => $port,
   check => "/usr/bin/curl -I http://localhost:$port",
   interval => "30s",
