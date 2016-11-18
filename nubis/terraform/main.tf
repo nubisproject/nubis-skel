31f73e75631ef2b27b4708c83bcf613aa7ca8042
limed <limed@mozilla.com>
2016-11-18 10:53:50 -0800

    Expose nubis_sudo_groups and nubis_user_groups

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -1,14 +1,16 @@
 module "worker" {
-  source       = "github.com/nubisproject/nubis-terraform//worker?ref=master"
-  region       = "${var.region}"
-  environment  = "${var.environment}"
-  account      = "${var.account}"
-  service_name = "${var.service_name}"
-  purpose      = "webserver"
-  ami          = "${var.ami}"
-  elb          = "${module.load_balancer.name}"
-  ssh_key_file = "${var.ssh_key_file}"
-  ssh_key_name = "${var.ssh_key_name}"
+  source            = "github.com/nubisproject/nubis-terraform//worker?ref=master"
+  region            = "${var.region}"
+  environment       = "${var.environment}"
+  account           = "${var.account}"
+  service_name      = "${var.service_name}"
+  purpose           = "webserver"
+  ami               = "${var.ami}"
+  elb               = "${module.load_balancer.name}"
+  ssh_key_file      = "${var.ssh_key_file}"
+  ssh_key_name      = "${var.ssh_key_name}"
+  nubis_sudo_groups = "${var.nubis_sudo_groups}"
+  nubis_user_groups = "${var.nubis_user_groups}"
 }
 
 module "load_balancer" {
